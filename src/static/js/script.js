a409d6ea106a31bdedb452357fe3231745053e3c
etienne cherri <etienne@cherri.fr>
2020-01-16 17:20:51 +0100

    test done clean code

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,5 +1,7 @@
-$(document).ready(function () {
-    $(window).on('load scroll', function () {
+// ES6
+// parallax effect
+$(document).ready(() => {
+    $(window).on('load scroll', () => {
         var scrolled = $(this).scrollTop();
         $('#title').css({
             'transform': 'translate3d(0, ' + -(scrolled * 0.2) + 'px, 0)', // parallax (20% scroll rate)
@@ -8,11 +10,11 @@ $(document).ready(function () {
         $('#hero-vid').css('transform', 'translate3d(0, ' + -(scrolled * 0.25) + 'px, 0)'); // parallax (25% scroll rate)
     });
 });
-
+​
 // image "natural slow life effect"
-
+​
 const parallax = document.querySelector('.parallax');
-
+​
 window.addEventListener('scroll', () => {
   parallax.style.backgroundPositionY = -window.scrollY / 2 + "px";
 });
